5cfe7230470efc46940e789f3d768ee72e1f8135
Pham Long <longpq18@gmail.com>
2019-08-05 22:20:22 +0700

    fix bug

--- android/src/main/java/com/reactlibrary/StartActivityOnBoot.java
+++ android/src/main/java/com/reactlibrary/StartActivityOnBoot.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
  
  
-public class StartActivityOnBootReceiver extends BroadcastReceiver {
+public class StartActivityOnBoot extends BroadcastReceiver {
  
     @Override
     public void onReceive(Context context, Intent intent) {
